7670e1635e6a7eb0731ce3e87096c2425454121f
oseias-silva-magalhaes-1990 <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2019-10-08 18:31:51 -0300

    Create ClasseAve.py

---
+++ algoritmos/Ex2/Codigo/ClasseAve.py
@@ -0,0 +1,21 @@
+from ReinoAnimal import Animal
+
+class Ave(Animal):
+
+	def __init__(self):
+		super().__init__()
+
+	def voar(self):
+		return 'estou voando'
+
+	def trocarPenas(self):
+		return 'estou trocando as penas'
+
+	def bicar(self):
+		return 'estou bicando'
+
+	def chocarOvos(self):
+		return 'estou chocando ovos'
+
+	
+
